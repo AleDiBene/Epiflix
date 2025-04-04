66cd3a94ab44512a783c858383cdb167f8d4020e
Alessandro Di Benedetto <socialorne02@gmail.com>
2025-04-04 11:41:43 +0200

    update

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -15,7 +15,6 @@ function Homepage() {
       .then((response) => response.json())
       .then((data) => {
         if (data.Search) {
-          // Controlliamo che la risposta contenga la proprietà "Search"
           setState(data.Search);
         } else {
           console.error("Errore: nessun film trovato o errore nell'API");
@@ -37,13 +36,60 @@ function Homepage() {
   }
 
   return (
-    <div className="text-white">
-      <h2>Harry Potter</h2>
-      <Carousel movies={harryPotter} />
-      <h2>Il Signore degli Anelli</h2>
-      <Carousel movies={lordOfTheRings} />
-      <h2>Star Wars</h2>
-      <Carousel movies={starWars} />
+    <div
+      className="text-white
+    "
+    >
+      <div className="container-fluid px-4">
+        <div className="d-flex justify-content-between">
+          <div className="d-flex">
+            <h2 className="mb-4">TV Shows</h2>
+            <div className="btn-group" role="group">
+              <div className="dropdown ms-4 mt-1">
+                <button
+                  type="button"
+                  className="btn btn-secondary btn-sm dropdown-toggle rounded-0"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  style={{ backgroundColor: "#221f1f" }}
+                >
+                  Genres
+                </button>
+                <ul className="dropdown-menu">
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Comedy
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Drama
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Thriller
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div>
+            <i className="bi bi-grid icons"></i>
+            <i className="bi bi-grid-3x3 icons"></i>
+          </div>
+        </div>
+
+        <h4>Harry Potter</h4>
+        <Carousel movies={harryPotter} />
+
+        <h4>Il Signore degli Anelli</h4>
+        <Carousel movies={lordOfTheRings} />
+
+        <h4>Star Wars</h4>
+        <Carousel movies={starWars} />
+      </div>
     </div>
   );
 }
